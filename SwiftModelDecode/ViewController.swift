f7c383aca9d4f60eb18e56b50136c2d9e4a4ada7
howie.zhang <howie.zhang@alo7.com>
2019-10-30 16:58:40 +0800

    Initial Commit

---
+++ SwiftModelDecode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftModelDecode
+//
+//  Created by 张和悦 on 2019/10/30.
+//  Copyright © 2019 ZHY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
